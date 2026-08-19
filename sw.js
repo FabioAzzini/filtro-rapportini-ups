4be4d8c8a0c1d5d3709359f1aeed0efc58b035d8
FabioAzzini <75931076+FabioAzzini@users.noreply.github.com>
2026-08-19 17:54:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='filtro-rapportini-ups-v6';
+const CACHE='filtro-rapportini-ups-v7';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

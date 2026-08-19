283c3112b14659f1c0693e294286ccc544414925
FabioAzzini <75931076+FabioAzzini@users.noreply.github.com>
2026-08-19 17:31:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE = 'filtro-rapportini-ups-v3';
+const CACHE = 'filtro-rapportini-ups-v4';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.min.mjs'
 ];
 
 self.addEventListener('install', event => {
